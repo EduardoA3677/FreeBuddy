8f1f0b8c8aca68d463aa9d7118dd4a1c249b1607
EduardoA3677 <ealvarado261500@gmail.com>
2025-03-28 22:52:46 -0600

    Update get_changelog.py

--- scripts/get_changelog.py
+++ scripts/get_changelog.py
@@ -1,26 +1,27 @@
 import argparse
 import re
 
+# Configuración del parser para los argumentos de la línea de comandos
 p = argparse.ArgumentParser()
-p.add_argument('--version', help='Version to get changelog for. You can type with or without "v" prefix')
+p.add_argument('--version', help='Version to get changelog for. You can type with or without "v" prefix', required=True)
 args = p.parse_args()
 
+# Eliminar el prefijo 'v' si está presente
 version = args.version.strip().replace('v', '')
-if not re.match(r'\d+.\d+.\d+', version):
+if not re.match(r'\d+\.\d+\.\d+', version):
     raise ValueError('Invalid version')
 
+# Abrir el archivo CHANGELOG.md
 with open("CHANGELOG.md", 'r') as f:
     lines = f.read()
 
-# Get first "##" followed by version
+# Buscar la versión correspondiente
 start = lines.index(f'## {version}')
-# Start from newline
 start = lines.index('\n', start) + 1
-# Find next "##" (previous version)
 try:
     end = lines.index('\n## ', start)
 except ValueError:
-    # in case there is no previous version
     end = -1
 
+# Imprimir el changelog de la versión
 print(lines[start:end].strip())
