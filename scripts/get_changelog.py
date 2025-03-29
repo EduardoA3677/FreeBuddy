1e6270ba5128c46e7cc18e2deaef3ba9c0bf65da
Eduardo <ealvarado261500@gmail.com>
2025-03-29 02:38:19 -0600

    Update build.yml and get_changelog.py for improved version handling and environment variable usage

--- scripts/get_changelog.py
+++ scripts/get_changelog.py
@@ -1,36 +1,29 @@
 import argparse
 import re
+import os
 
-# Configuración del parser para los argumentos de la línea de comandos
 p = argparse.ArgumentParser()
-p.add_argument('--version', help='Version to get changelog for. You can type with or without "v" prefix', required=True)
+p.add_argument('--version', help='Version to get changelog for. You can type with or without "v" prefix')
 args = p.parse_args()
 
-# Eliminar el prefijo 'v' si está presente
 version = args.version.strip().replace('v', '')
-if not re.match(r'\d+\.\d+\.\d+', version):
+if not version:
+    version = os.getenv('tag', '').strip().replace('v', '')
+if not re.match(r'\d+.\d+.\d+', version):
     raise ValueError('Invalid version')
 
-# Debugging statement
-print(f'Debug: Looking for changelog version {version}')
-
-# Abrir el archivo CHANGELOG.md
 with open("CHANGELOG.md", 'r') as f:
     lines = f.read()
 
-# Buscar la versión correspondiente
+# Get first "##" followed by version
+start = lines.index(f'## {version}')
+# Start from newline
+start = lines.index('\n', start) + 1
+# Find next "##" (previous version)
 try:
-    start = lines.index(f'## {version}')
-    start = lines.index('\n', start) + 1
-    try:
-        end = lines.index('\n## ', start)
-    except ValueError:
-        end = len(lines)
-
-    # Debugging statement
-    print(f'Debug: Start index: {start}, End index: {end}')
-    
-    # Imprimir el changelog de la versión
-    print(lines[start:end].strip())
+    end = lines.index('\n## ', start)
 except ValueError:
-    print(f'Error: Version {version} not found in CHANGELOG.md')
+    # in case there is no previous version
+    end = -1
+
+print(lines[start:end].strip())
