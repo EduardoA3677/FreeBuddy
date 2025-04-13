357bd6dccbe3d01f4e8a5646b5faeba27edcfc2b
EduardoA3677 <ealvarado261500@gmail.com>
2025-04-12 22:43:06 -0600

    Update settings_page.dart

--- lib/ui/pages/settings/settings_page.dart
+++ lib/ui/pages/settings/settings_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
